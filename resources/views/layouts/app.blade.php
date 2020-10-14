c741a75c6112bcf826711b5dea815b5bb2bfdb95
Litlife <litlife.club@gmail.com>
2020-10-14 19:32:51 +0300

    ya metrika

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -282,19 +282,21 @@
 
 @stack('after_scripts')
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-172840850-1"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-
-    gtag('js', new Date());
-
-    gtag('config', 'UA-172840850-1');
+    ym(68301934, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+    });
 </script>
+<noscript><div><img src="https://mc.yandex.ru/watch/68301934" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 
 </body>
 </html>
