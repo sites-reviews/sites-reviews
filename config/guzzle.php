9741203c4fff256b1359114bc1169d26c96d1608
Litlife <litlife.club@gmail.com>
2021-01-07 16:45:34 +0300

    guzzle config updated

--- config/guzzle.php
+++ config/guzzle.php
@@ -11,9 +11,22 @@ return [
                 'protocols' => ['http', 'https']
             ],
             'headers' => [
-                'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'Accept-Encoding' => 'gzip, deflate, br',
-                'Accept-Language' => 'en-EN,ru-RU;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Accept' => [
+                    'text/html',
+                    'application/xhtml+xml',
+                    'application/xml'
+                ],
+                'Accept-Encoding' => [
+                    'gzip',
+                    'deflate',
+                    'br'
+                ],
+                'Accept-Language' => [
+                    'en-EN',
+                    'ru-RU',
+                    'en-US',
+                    'en'
+                ],
                 'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
             ]
         ]
